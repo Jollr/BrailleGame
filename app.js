be3f79c79ece994f79d45c7b3b9e5c01f968f272
Jollr <jolle.lont@gmail.com>
2016-06-12 10:28:46 +0200

    added input module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(function() {
 	var dispatcher = new Dispatcher();
-	
+	var input = new Input(dispatcher);
+
 	dispatcher.Publish('Initialize');
 });
---
+++ input.js
@@ -0,0 +1,26 @@
+﻿var Input = function(dispatcher) {
+	var initialize = function() {
+		document.onkeydown = function(e) {
+
+			if (e.ctrlKey && e.shiftKey && e.keyCode == 32) { // ctrl + shift + spatiebalk
+				dispatcher.Publish('MagicHotkey', {});
+			}
+
+			if (e.keyCode >= 65 && e.keyCode <= 90) { 
+				dispatcher.Publish('LetterTyped', { letter: e.key.toUpperCase() });
+			}
+			
+			if (e.keyCode == 8) { // backspace
+				dispatcher.Publish('Backspace', { });
+			}
+
+			if (e.keyCode == 13) { // enter
+				dispatcher.Publish('Submit', { });
+			}
+			
+			return false;
+		};
+	};
+	
+	dispatcher.Subscribe('Initialize', initialize);
+};
